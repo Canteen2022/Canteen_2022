24b1c6385ff5ad0f382eeab23c095f4886d9ca5b
Marc-Michot-NXO-France <32137401+Marc-Michot-NXO-France@users.noreply.github.com>
2022-07-24 15:02:00 +0200

    added an entity + migration class ; BackEnd Side

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,20 +1,42 @@
-import { AuthService } from '@abp/ng.core';
-import { Component } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
+import { ToasterService } from "@abp/ng.theme.shared";
+import { Component, OnInit } from '@angular/core';
+import { CalendarItemDto } from "@proxy/services/dtos";
+import { CalendarService } from "@proxy/services";
+import { DateTimeFormatDto } from "@abp/ng.core";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  get hasLoggedIn(): boolean {
-    return this.oAuthService.hasValidAccessToken();
+
+export class HomeComponent implements OnInit {
+
+  CalendarItems: CalendarItemDto[];
+  newCalendarDate: DateTimeFormatDto;
+
+  constructor(
+      private CalendarService: CalendarService,
+      private toasterService: ToasterService)
+  { }
+
+  ngOnInit(): void {
+    this.CalendarService.getList().subscribe(response => {
+      this.CalendarItems = response;
+    });
+  }
+  
+  create(): void{
+    this.CalendarService.create(this.newCalendarDate).subscribe((result) => {
+      this.CalendarItems = this.CalendarItems.concat(result);
+      this.newCalendarDate = null;
+    });
   }
 
-  constructor(private oAuthService: OAuthService, private authService: AuthService) {}
-
-  login() {
-    this.authService.navigateToLogin();
-  }
+  delete(id: string): void {
+    this.CalendarService.delete(id).subscribe(() => {
+      this.CalendarItems = this.CalendarItems.filter(item => item.id !== id);
+      this.toasterService.info('Deleted the Calendar item.');
+    });
+  }  
 }
--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [SharedModule, HomeRoutingModule],
+  imports: [
+    SharedModule, 
+    HomeRoutingModule,
+    MatDatepickerModule
+  ],
 })
 export class HomeModule {}
